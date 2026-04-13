ceb314d6540e78177489a94c7dee7c939b39efd2
Tein <teinbrouwer@gmail.com>
2026-04-13 10:25:49 +0000

    v71: all items now have links, fixed SW cache, bumped version

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,12 @@
-const CACHE = "italia-v1";
+const CACHE = "italia-v70";
 const URLS = ["/"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS))));
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
 self.addEventListener("fetch", e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
